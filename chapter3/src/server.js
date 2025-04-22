0b01e4475a32dbb2a7892e665df53b15be70321f
afthegreat <abelferede916@gmail.com>
2025-04-22 11:59:11 +0300

    server responses for front end user to-do app

---
+++ chapter3/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
+
+const app=express()
+const PORT= process.env.PORT || 5003
+//url to path allocation
+const __filename=fileURLToPath(import.meta.url)
+//telling the direcotry of the path
+const __dirname=dirname(__filename)
+app.use(express.static(path.join(__dirname, '../public')))
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.listen(PORT,()=>
+{
+    console.log(`server running on port ${PORT}`)
+}
+    )
