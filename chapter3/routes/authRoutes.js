14cddd7ffdfc896ee0ef4ec87c9bac934a8f614e
afthegreat <abelferede916@gmail.com>
2025-04-24 12:04:18 +0300

    test case and jwt from .env file and rergistration to database are added

--- chapter3/routes/authRoutes.js
+++ chapter3/routes/authRoutes.js
@@ -4,10 +4,32 @@ import jwt from 'jsonwebtoken'
 import db from '../src/db.js'
 
 const router =express.Router()
+//end point that handles requestes for registration
 router.post('/register',(req,res)=>{
+    console.log('Received body:', req.body)
+    //accepts the usen name and password from front end or requester
     const {username,password}=req.body
+    //encrypts the password through bcrypt
     const hashedpassword=bcrypt.hashSync(password,8)
-    console.log(hashedpassword)
+   try{
+    //adding new user to the database
+const insertUser=db.prepare(`INSERT INTO users (username, password) VALUES (?,?)`)
+const result=insertUser.run(username,hashedpassword)
+//add first to do task for each user
+const defaultTodo='Hello :) Add your first todo! '
+const insertTodo= db.prepare(`INSERT INTO todos (user_id, task) VALUES(?,?)`)
+   insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+   //create a token
+   const token=jwt.sign({ id: result.lastInsertRowid}, process.env.
+    JWT_SECRET,{expiresIn:'24h'}
+   )
+   res.json({token})
+}
+   catch(err){
+    console.log(err.message)
+    res.sendStatus(400)
+   }
    
 })
 router.post('/login', (req,res)=>{
--- chapter3/src/server.js
+++ chapter3/src/server.js
@@ -4,8 +4,10 @@ import { fileURLToPath } from 'url'
 import authRoutes from '../routes/authRoutes.js'
 import todoRoutes from '../routes/todoRoutes.js';  // Correct import
 
+
 const app=express()
-const PORT= process.env.PORT || 5003
+const PORT= process.env.PORT ||5003
+app.use(express.json())
 //url to path allocation
 const __filename=fileURLToPath(import.meta.url)
 //telling the direcotry of the path
@@ -13,7 +15,7 @@ const __dirname=dirname(__filename)
 //middleware to tell from where to use the directory
 app.use(express.static(path.join(__dirname, '../public')))
 //middleware to use json files
-app.use(express.json())
+
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
