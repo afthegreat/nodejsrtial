4bff16c9b8435067d7a7d93654cf3db792793548
afthegreat <abelferede@gmail.com>
2025-06-16 16:54:59 +0300

    login functionality started

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContent } from '../context/AppContext'
+import axios from 'axios'
 
 export const Login = () => {
   const navigate= useNavigate()
+  const {backendUrl, setIsLoggedin}=useContext(AppContent)
 
   const [state, setstate]=useState('Sign Up')
   const [name, setName]=useState('')
   const [email, setEmail]=useState('')
   const [password, setPassword]=useState('')
 
+const onSubmitHandler= async (e)=>{
+  try{
+    e.preventDefault();
 
+    axios.defaults.withCredentials=true
+    if(state===('Sign Up')){
+   const data= await axios.post(backendUrl+'/api/auth/register', {name, email, password})
+      if(data.success){
+        setIsLoggedin(true)
+        navigate('/')
+      }else
+      {
+        alert(data.message)
+      }
+    }
+
+  }catch (error)
+}
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0
     bg-gradient-to-br from-blue-200 to-puple-400'>
@@ -22,7 +42,7 @@ export const Login = () => {
      
         <p className='text-center text-sm mb-6'>   {state==='Sign Up' ? 'Create your account': 'Login to your account'}
         </p>  
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {state==='Sign Up'&& (       
                <div className='flex items-center mb-4 gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.person_icon} alt=''/>
